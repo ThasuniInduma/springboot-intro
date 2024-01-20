37e11654a0a82270942c7a67d546cac496facc1c
Thasuni Induma <thasuniinduma@gmail.com>
2024-01-20 03:44:22 -0800

    JWT Auth

--- src/main/java/com/ijse/database/controller/AuthController.java
+++ src/main/java/com/ijse/database/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
